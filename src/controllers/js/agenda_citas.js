8b6d2c7d728110d9b7ec89eb8ae1ca3ec167ea60
Daniel Chaparro <daniel.marin92@hotmail.com>
2022-10-27 07:08:35 -0500

    .

--- src/controllers/js/agenda_citas.js
+++ src/controllers/js/agenda_citas.js
@@ -1,4 +1,16 @@
+
+import { muestraError } from "../lib/js/muestraError.js";
+import { submit } from "../lib/js/submit.js";
+
+
 document.addEventListener('DOMContentLoaded', () => cargarCitas);
 
-async function cargarCitas() {
+/** @param {Event} evt */
+async function cargarCitas(evt) {
+    try {
+        const json = await submit(evt, "../lib/php/leerCitas.php", document);
+        alert(json.resultado);
+    } catch (e) {
+        muestraError(e);
+    }
 }
