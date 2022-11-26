04d7d3674350d9b61f87cddc650dab41b9bede0f
elyesos10 <108501019+elyesos10@users.noreply.github.com>
2022-11-25 23:07:53 -0600

    Update servicios.php
    Barra de busqueda de servicios

--- pages/servicios.php
+++ pages/servicios.php
@@ -16,6 +16,9 @@
     
     <script src="../src/js/index.js" defer></script>
     <script src="https://kit.fontawesome.com/4ad7b82c7d.js" crossorigin="anonymous" defer></script>
+     <!-- bootstrap -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous" defer></script>
 
 </head>
 <body>
@@ -134,6 +137,12 @@
                     </div>
                 </div>
             </section>
+
+            <div class="buscador__container">
+        <form class="d-flex" method="GET">
+            <input name="busqueda" class="form-control me-2" type="text" placeholder="Buscar por Servicio" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+        </form>
 
             <div class="header__title">
                 <h1 id="header__title-h1">Servicios de estética <br /> y atención médica😽</h1>
