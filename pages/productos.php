97c821ca2be782fecf8407744dc75875d2fc42ee
adriana22410 <108551425+adriana22410@users.noreply.github.com>
2022-11-28 00:19:03 -0600

    Actualizacion

--- pages/productos.php
+++ pages/productos.php
@@ -114,7 +114,7 @@ $resultado= $sql->fetchAll(PDO::FETCH_ASSOC);
                 <div class="btn-group">
                 <a href="detalles.php?id=<?php echo $row['id']; ?>&token=<?php echo hash_hmac('sha1', $row['id'], KEY_TOKEN);?>" class="btn btn-primary"> Detalles </a>
                 </div>
-                <button class="btn btn-outline-success" type="button" onclick="addProducto(<?php echo $row['id']; ?>,'<?php echo hash_hmac('sha1', $row['id'], KEY_TOKEN);?>')"  >Agregar al carrito</button>
+               <a href="productos.php"> <button class="btn btn-outline-success" type="button" onclick="addProducto(<?php echo $row['id']; ?>,'<?php echo hash_hmac('sha1', $row['id'], KEY_TOKEN);?>')"  >Agregar al carrito</button></a>
               </div>
             </div>
           </div>
